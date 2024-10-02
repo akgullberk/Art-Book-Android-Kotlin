824d33999a5d09abdf2ef51cd11295466ddd25aa
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-02 15:37:27 +0300

    uses permission

--- app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
@@ -1,12 +1,19 @@
 package com.example.artbookkotlin
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.artbookkotlin.databinding.ActivityArtBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArtActivity : AppCompatActivity() {
 
@@ -30,6 +37,21 @@ class ArtActivity : AppCompatActivity() {
     }
 
     fun selectImage(view : View){
+
+        if(ContextCompat.checkSelfPermission(this@ArtActivity,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",View.OnClickListener {
+
+                }).show()
+            }else{
+
+            }
+        }
+        else{
+            val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        }
 
     }
 }
