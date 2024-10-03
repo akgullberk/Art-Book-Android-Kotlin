ee41dee38fc13a415a4247dca6aba1cfa374a154
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-03 18:06:37 +0300

    registerLauncher

--- app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
@@ -3,10 +3,16 @@ package com.example.artbookkotlin
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -18,6 +24,10 @@ import com.google.android.material.snackbar.Snackbar
 class ArtActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityArtBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedBitmap : Bitmap? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +40,14 @@ class ArtActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        registerLauncher()
+
+
     }
 
     fun saveButtonClicked(view: View){
+
 
     }
 
@@ -42,16 +57,66 @@ class ArtActivity : AppCompatActivity() {
             if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
 
                 Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",View.OnClickListener {
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+
 
                 }).show()
             }else{
-
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
             }
         }
         else{
             val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+
 
         }
 
     }
+
+    private fun registerLauncher(){
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if(result.resultCode == RESULT_OK){
+                val intentFromResult = result.data
+                if(intentFromResult != null){
+                    val imageData = intentFromResult.data
+                    //binding.imageView.setImageURI(imageData)
+                    if(imageData != null){
+                        try {
+                            if(Build.VERSION.SDK_INT >= 28){
+                                val source = ImageDecoder.createSource(this@ArtActivity.contentResolver,imageData)
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }else{
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(contentResolver,imageData)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                        } catch (e: Exception){
+                            e.printStackTrace()
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if(result){
+
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+
+            }else{
+                Toast.makeText(this@ArtActivity,"Permission needed!",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+
 }
