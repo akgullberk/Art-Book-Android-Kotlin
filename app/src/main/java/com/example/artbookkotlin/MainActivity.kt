d914201515395ca0182535b1cb7e857157f34289
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-06 12:57:15 +0300

    RecyclerView Adapter

--- app/src/main/java/com/example/artbookkotlin/MainActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/MainActivity.kt
@@ -8,11 +8,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.artbookkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var artList : ArrayList<Art>
+    private lateinit var artAdapter: ArtAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,35 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        artList = ArrayList<Art>()
+        artAdapter = ArtAdapter(artList)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = artAdapter
+
+        try {
+            val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+
+            val cursor = database.rawQuery("SELECT * FROM arts", null)
+            val artNameIx = cursor.getColumnIndex("artname")
+            val idIx = cursor.getColumnIndex("id")
+
+            while(cursor.moveToNext()){
+                val name = cursor.getString(artNameIx)
+                val id = cursor.getInt(idIx)
+                val art = Art(name, id)
+                artList.add(art)
+
+            }
+
+            artAdapter.notifyDataSetChanged()
+
+            cursor.close()
+
+        }catch (e: Exception){
+            e.printStackTrace()
         }
 
 
