a9f0b075139afb2ed6c68bc6bdc83d8ba74f836e
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-06 21:11:10 +0300

    intent

--- app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/ArtActivity.kt
@@ -3,7 +3,9 @@ package com.example.artbookkotlin
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.sqlite.SQLiteDatabase
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageDecoder
 import android.os.Build
 import android.os.Bundle
@@ -28,6 +30,7 @@ class ArtActivity : AppCompatActivity() {
     private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
     var selectedBitmap : Bitmap? = null
+    private lateinit var database : SQLiteDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +45,45 @@ class ArtActivity : AppCompatActivity() {
             insets
         }
 
+        database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+
         registerLauncher()
+
+        val intent = intent
+        val info = intent.getStringExtra("info")
+        if(info.equals("new")){
+            binding.artNameText.setText("")
+            binding.artistNameText.setText("")
+            binding.yearText.setText("")
+            binding.button.visibility = View.VISIBLE
+            binding.imageView.setImageResource(R.drawable.selectimage)
+        }else{
+            binding.button.visibility = View.INVISIBLE
+            val selectedId = intent.getIntExtra("id",1)
+
+            val cursor = database.rawQuery("SELECT * FROM arts WHERE id = ?", arrayOf(selectedId.toString()))
+
+            val artNameIx = cursor.getColumnIndex("artname")
+            val artistNameIx = cursor.getColumnIndex("artistname")
+            val yearIx = cursor.getColumnIndex("year")
+            val imageIx = cursor.getColumnIndex("image")
+
+            while(cursor.moveToNext()){
+                binding.artNameText.setText(cursor.getString(artNameIx))
+                binding.artistNameText.setText(cursor.getString(artistNameIx))
+                binding.yearText.setText(cursor.getString(yearIx))
+
+                val byteArray =  cursor.getBlob(imageIx)
+                val bitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.size)
+                binding.imageView.setImageBitmap(bitmap)
+
+            }
+
+            cursor.close()
+
+
+
+        }
 
 
     }
@@ -61,7 +102,7 @@ class ArtActivity : AppCompatActivity() {
             val byteArray = outputStream.toByteArray()
 
             try {
-                val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+
                 database.execSQL("CREATE TABLE IF NOT EXISTS arts (id INTEGER PRIMARY KEY, artname VARCHAR, artistname VARCHAR, year VARCHAR, image BLOB)")
                 val sqlString = "INSERT INTO arts (artname, artistname, year, image) VALUES (?, ?, ?, ?)"
                 val statement = database.compileStatement(sqlString)
--- app/src/main/java/com/example/artbookkotlin/MainActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/MainActivity.kt
@@ -72,6 +72,7 @@ class MainActivity : AppCompatActivity() {
 
         if(item.itemId == R.id.add_art_item){
             val intent = Intent(this@MainActivity,ArtActivity::class.java)
+            intent.putExtra("info","new")
             startActivity(intent)
         }
 
